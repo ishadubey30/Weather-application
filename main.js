c3cf3bfd96caa9ba7b9b790305cc01c0154cc874
Isha Dubey <ishadubey36@gmail.com>
2023-09-28 11:24:46 +0530

    .

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
  const API_KEY = "137d7f888186063e2802880671682c13"
+
+ const DAYS_OF_THE_WEEK = ["sun" , "mon" , "tue" , "wed" , "thu" , "fri" , "sat"]
+
  const getCurrentWeatherData = async () =>{
      const city = "pune"
      const response = await 
@@ -13,7 +16,7 @@
     return {temp , temp_max , temp_min , dt , dt_txt, description ,icon }
    })
  }
-// const 
+
  const loadHourlyForecast = (hourlyForecast) => { 
     console.log(hourlyForecast)
     let dataForTwelveHours = hourlyForecast.slice(1,13)
@@ -30,14 +33,37 @@
 
  }
 
+ const calculateDayWiseForecast = (hourlyForecast) => {
+    let dayWiseForcast = new Map()
+    for (let forecast of hourlyForecast){
+        const [date] = forecast.dt_txt.split(" ")
+        const dayOfTheWeek = DAYS_OF_THE_WEEK(new Date(date).getDay) // getday is a function which tells that what
+        // is the day monday means 1 tuesday mean 2
+        console.log(dayOfTheWeek)
+        if (dayWiseForcast.has(dayOfTheWeek)){
+            let forecastForTheDay = dayWiseForcast.get(dayOfTheWeek)
+            forecastForTheDay.push(forecast);
+            dayWiseForcast.set(dayOfTheWeek ,  forecastForTheDay)
+        } else{
+            dayWiseForcast.set(dayOfTheWeek , [forecast])    
+        }
+    }
+    console.log(dayWiseForcast)
+    for (let [key , value] of dayWiseForcast) {
+        let minTemperature = Math.min(...Array.from(value , val => val.temp_min))
+        let  maxTemperature = Math.max(...Array.from(value , val => val.temp_max))
 
-const loadfeelslike = ({main : {feels_like}}) =>{
-   let container = document.querySelector("#feels-like") 
-   container.querySelector(".feelslike-temp").textContent = FormatTemperature(feels_like)}
+        dayWiseForcast.set(key , {temp_min , temp_max , icon : value.find( v => v.icon)})
+    } 
+    console.log(dayWiseForcast)
+ }
 
-   const humidity = ({main : { humidity}}) =>{
-    let container = document.querySelector("#humidity") 
-    container.querySelector(".humid").textContent = ` ${humidity} % ` }
+ const loadFiveDayForecast = (hourlyForecast) => {
+    console.log(hourlyForecast)  //we have multiple entries for a day eg 6pm 9pm so we have to group the entries together and use 
+    //min and max temp of the entries . we will group the information date wise and for that we will use map.
+
+    const dayWiseForcast = calculateDayWiseForecast(hourlyForecast)
+ }
 
 const createIconUrl = (icon) => `https://openweathermap.org/img/wn/${icon}@2x.png`
 const FormatTemperature = (temp) => `${temp?.toFixed(1)}゜`
@@ -50,6 +76,19 @@ const loadCurrentForecast = ({ name, main: { temp, temp_max, temp_min }, weather
     currentForecastElement.querySelector(".description").textContent = description;
     currentForecastElement.querySelector(".min-max-temp").textContent = `H : ${FormatTemperature(temp_max)} L: ${FormatTemperature(temp_min)}`;
 };
+
+
+
+const loadfeelslike = ({main : {feels_like}}) =>{
+    let container = document.querySelector("#feels-like") 
+    container.querySelector(".feelslike-temp").textContent = FormatTemperature(feels_like)}
+ 
+    const humidity = ({main : { humidity}}) =>{
+     let container = document.querySelector("#humidity") 
+     container.querySelector(".humid").textContent = ` ${humidity} % ` 
+    }
+ 
+
  
  document.addEventListener("DOMContentLoaded" , async () => {
      const currentWeather = await getCurrentWeatherData()
@@ -60,3 +99,5 @@ const loadCurrentForecast = ({ name, main: { temp, temp_max, temp_min }, weather
 humidity(currentWeather)
 
  })
+
+ 
